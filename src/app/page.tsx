939feaf6aefec44083c316abf20b795babc1a45c
danieltinois <danieltinois@gmail.com>
2025-12-07 01:59:44 -0300

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Hello to my web-site</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+        fontFamily: {
+            sans: "var(--font-sans)",
+            mono: "var(--font-mono)",
+        }
+}
